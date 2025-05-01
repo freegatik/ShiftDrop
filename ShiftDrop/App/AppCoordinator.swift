39164d73888efa35d8a8da3d5b29b8a3f2be6d7d
freegatik <anton.soloviev2016@gmail.com>
2025-05-01 09:42:32 +0300

    Introduce AppCoordinator for root navigation shell

---
+++ ShiftDrop/App/AppCoordinator.swift
@@ -0,0 +1,55 @@
+//
+//  AppCoordinator.swift
+//  ShiftDrop
+//
+//  Created by Anton Solovev on 01.05.2025.
+//
+
+import UIKit
+
+final class AppCoordinator {
+    private let dependencies: AppDependencies
+    private let window: UIWindow
+
+    init(window: UIWindow, dependencies: AppDependencies) {
+        self.window = window
+        self.dependencies = dependencies
+    }
+
+    func start() {
+        let calculation = ViewController(dependencies: dependencies)
+        calculation.tabBarItem = UITabBarItem(
+            title: Localized.tabCalculation,
+            image: UIImage(named: "calculate.pdf"),
+            tag: 0
+        )
+
+        let history = SecondViewController(dependencies: dependencies)
+        history.tabBarItem = UITabBarItem(
+            title: Localized.tabHistory,
+            image: UIImage(named: "time.pdf"),
+            tag: 1
+        )
+
+        let profile = ThirdViewController(dependencies: dependencies)
+        profile.tabBarItem = UITabBarItem(
+            title: Localized.tabProfile,
+            image: UIImage(named: "user.pdf"),
+            tag: 2
+        )
+
+        let tabBarController = UITabBarController()
+        tabBarController.setViewControllers([calculation, history, profile], animated: false)
+        tabBarController.selectedIndex = 0
+        styleTabBar(tabBarController.tabBar)
+
+        window.rootViewController = tabBarController
+        window.backgroundColor = .white
+        window.makeKeyAndVisible()
+    }
+
+    private func styleTabBar(_ tabBar: UITabBar) {
+        tabBar.barTintColor = .white
+        tabBar.backgroundColor = .white
+    }
+}
